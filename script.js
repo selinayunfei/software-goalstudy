ac0d9b9c963765a7f5cdbed16f50a5b6ee997b59
Selina Yunfei <selinayunfei@gmail.com>
2026-06-28 17:49:45 -0400

    api'ed up!

---
+++ script.js
@@ -0,0 +1,199 @@
+
+
+// coding different pages
+function switchTab(tabId) {
+    const sections = document.querySelectorAll('[class*="page"]');
+    sections.forEach(section => {
+        section.classList.remove('active');
+    });
+    document.getElementById(tabId).classList.add('active');
+}
+
+const Football = document.querySelector('#football-button');
+const FormulaOne = document.querySelector('#formula1-button');
+const FigSkating = document.querySelector('#figskating-button');
+
+Football.addEventListener('click', () =>{
+    switchTab('football');
+});
+FormulaOne.addEventListener('click', () => {
+    switchTab('formula1');
+});
+FigSkating.addEventListener('click', () => {
+    switchTab('figskating');
+});
+
+
+
+const apiKey = "ce6f5e66ea3dd490c999a5fc1bfcf848";
+
+async function fetchLiveScores() {
+  const headers = new Headers();
+  headers.append("x-apisports-key", apiKey);
+
+  const requestOptions = {
+    method: "GET",
+    headers: headers,
+    redirect: "follow"
+  };
+
+  try {
+
+    const response = await fetch(
+      "https://v3.football.api-sports.io/fixtures?league=1&live=all",
+      requestOptions
+    );
+
+    const data = await response.json();
+    displayScores(data.response);
+
+  } catch (error) {
+    console.error("Error fetching live scores:", error);
+  }
+}
+
+function displayScores(matches) {
+    if (matches.length === 0) return;
+
+    const match = matches[0];
+
+    const homeTeam = match.teams.home.name;
+    const awayTeam = match.teams.away.name;
+
+    const homeGoals = match.goals.home;
+    const awayGoals = match.goals.away;
+    const elapsedMinutes = match.fixture.status.elapsed;
+
+    foloaddata(
+        homeTeam, 
+        awayTeam,
+        homeGoals,
+        awayGoals
+    );
+
+    countTimer(elapsedMinutes);
+}
+
+// for football
+function foloaddata(countryone, countrytwo, score1, score2){
+    document.getElementById("country-1").src = 
+        `media/flags/${countryone}.jpg`;
+    document.getElementById("country-2").src =
+        `media/flags/${countrytwo}.jpg`;
+
+    document.getElementById("country-1-score").textContent = 
+        score1;
+    document.getElementById("country-2-score").textContent = 
+        score2;
+};
+
+// timer 
+
+//let totalSeconds = 0;
+let timerVar;
+
+let elapsed = 0;
+let elapsedSeconds = 0;
+let halftimeDone = false;
+
+function startTimer(initialElapsed) {
+    clearInterval(timerVar);
+
+    elapsedSeconds = initialElapsed * 60;
+    updateDisplay();
+
+    timerVar = setInterval(() => {
+        elapsedSeconds++;
+        elapsed = Math.floor(elapsedSeconds/60);
+        updateDisplay();
+    }, 1000);
+}
+
+function updateDisplay() {
+
+    let hour = Math.floor(elapsed / 60);
+    let minute = elapsed % 60;
+    let seconds = elapsedSeconds % 60;
+
+
+    let h = String(hour).padStart(2,'0');
+    let m = String(minute).padStart(2,'0');
+    let s = String(seconds).padStart(2,'0');
+
+    document.getElementById("timer").innerHTML = `${h}:${m}:${s}`;
+    document.getElementById("minuter").innerHTML = `${elapsed}:${s}`;
+
+    if (elapsed < 45) {
+        document.getElementById("halfno").textContent =
+            "FIRST HALF";
+    } else if (elapsed == 45 && !halftimeDone) {
+        halftimeDone = true;
+
+        document.getElementById("halfno").textContent =
+            "HALF TIME";
+
+        clearInterval(timerVar);
+    } else if (elapsed >= 46){
+        document.getElementById("halfno").textContent = 
+            "SECOND HALF";
+    } 
+};
+
+setInterval(fetchLiveScores, 240000);
+
+// for f1 TO BE UPDATED; mockup
+const directory = [
+{
+    team: "Red Bull",
+    file: "media/f1 teams/redbull.png",
+    colour: "rgba(53, 113, 198)"
+
+},
+{
+    team: "Ferrari",
+    file: "media/f1 teams/ferrari.png",
+    colour: "rgba(232, 1, 45)"
+},
+{
+    team: "Mercedes",
+    file: "media/f1 teams/mercedes.png",
+    colour: "rgba(38, 239, 206)"
+},
+{
+    team: "McLaren",
+    file: "media/f1 teams/mclaren.png",
+    colour: "rgba(255, 128, 1)"
+}
+]
+
+
+function f1loaddata(){
+    document.getElementById("p1-team").src =
+        directory[2].file;
+    document.getElementById("team1-icon").style.backgroundColor = 
+        directory[2].colour;
+
+    document.getElementById("p2-team").src = 
+        directory[0].file;
+    document.getElementById("team2-icon").style.backgroundColor = 
+        directory[0].colour;
+
+    document.getElementById("p3-team").src =
+        directory[2].file;
+    document.getElementById("team3-icon").style.backgroundColor = 
+        directory[2].colour;
+
+
+    document.getElementById("p1-driver").textContent =
+        "George Russell";
+    document.getElementById("p2-driver").textContent = 
+        "Max Verstappen";
+    document.getElementById("p3-driver").textContent = 
+        "Kimi Antonelli";
+
+    document.getElementById("lap-no").textContent =
+        "71";
+    document.getElementById("time").textContent = 
+        "1:26:37.979";
+}
+f1loaddata();
